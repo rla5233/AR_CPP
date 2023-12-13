5224e9fd708eb31afaf13add931ec641c8b2e1e5
AssortRock0 <AssortRock0@ASSORTROCK>
2023-12-13 16:47:02 +0900

    커밋 연습1

--- Cpp/0000_Free/0_Free.cpp
+++ Cpp/0000_Free/0_Free.cpp
@@ -3,9 +3,6 @@
 #include <vector>
 using namespace std;
 
-void Hotel();
-void CountRoom(int n, int m);
-bool CheckRoom(int n);
 
 int main()
 {
@@ -13,36 +10,4 @@ int main()
     Hotel();
 
     return 0;
-}
-
-void Hotel()
-{
-    int N, M;
-    while (1)
-    {
-        cin >> N >> M;
-        if (cin.eof())
-            break;
-
-        CountRoom(N, M);
-    }
-}
-
-void CountRoom(int n, int m)
-{
-    int count = 0;
-    for (int i = n; i <= m; i++)
-    {
-        if (CheckRoom(i))
-            count++;
-    }
-
-    cout << count << "\n";
-}
-
-    // int 최소값
-    //n = i ^ (0b01111111111111111111111111111111);
-    //cout << n << endl;
-
-    return true;
 }
