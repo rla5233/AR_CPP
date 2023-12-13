c996360573ddfcb8abc2908df6c21b473b0fb267
AssortRock0 <AssortRock0@ASSORTROCK>
2023-12-13 12:51:11 +0900

    # rla5233 - 2023.12.13 중간정리

---
+++ Cpp/0000_Free/0_Free.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void Hotel();
+void CountRoom(int n, int m);
+bool CheckRoom(int n);
+
+int main()
+{
+    //ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    Hotel();
+
+    return 0;
+}
+
+void Hotel()
+{
+    int N, M;
+    while (1)
+    {
+        cin >> N >> M;
+        if (cin.eof())
+            break;
+
+        CountRoom(N, M);
+    }
+}
+
+void CountRoom(int n, int m)
+{
+    int count = 0;
+    for (int i = n; i <= m; i++)
+    {
+        if (CheckRoom(i))
+            count++;
+    }
+
+    cout << count << "\n";
+}
+
+bool CheckRoom(int n)
+{
+    vector<bool> v(10, false);
+    string temp = to_string(n);
+    for (int j = 0; j < temp.length(); j++)
+    {
+        if (!v[temp[j] - '0'])
+            v[temp[j] - '0'] = true;
+        else
+            return false;
+    }
+
+    return true;
+}
---
+++ Cpp/007_GlobalVar/007_GlobalVar.cpp
@@ -0,0 +1,18 @@
+﻿// 007_GlobalVar.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 전역 변수
+int MonsterSpeed = 10;
+
+void MonsterMove()
+{
+	// 지역 변수
+	int MonsterSpeed = 5;
+}
+
+int main()
+{
+	MonsterMove();
+}
