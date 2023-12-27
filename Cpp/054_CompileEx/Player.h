ed32310a59e37e7cfca33754b94edcfb34916af4
rla5233 <rla5233@naver.com>
2023-12-27 14:51:50 +0900

    # 2023.12.27 중간정리

--- Cpp/054_CompileEx/Player.h
+++ Cpp/054_CompileEx/Player.h
@@ -1,6 +1,19 @@
 #pragma once
+// #include "Monster.h"
+// 헤더에서 헤더참조를 최대한 피하지만 극단적으로 가면 안된다.
+// 이건 경험이다.
+// 어쩔수 없을때는 순환참조를 주의하면서 헤더 추가하면 된다.
 
 
 class Player
-{};
+{
+	// 전방선언
+	class Monster* NewMonster;
+
+	void Test()
+	{
+		
+	}
+
+};
 
---
+++ Cpp/055_inheritance/055_inheritance.cpp
@@ -0,0 +1,90 @@
+﻿// 055_inheritance.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 둘다 Att, Hp 갖네...
+// 두번 치는건 불합리해..
+
+// 문법의 발전은 모두다 프로그래밍 코드의 재활용성을
+// 증가 시키기 위해서 발전해 왔다.
+// 코드 재활용성 => 두번치기 귀찮네. 복붙도 귀찮네.
+
+// 상속?
+// 코드 재활용성을 위해
+
+// XXXX분야의 클래스들이 공통적으로 가져야할 기능을 가진다.
+
+
+// 싸우는 애들이 공통적으로 가져야할 기능을 가진다.
+class FightUnit
+{
+public:
+    void Damage(int _Att)
+    {
+        Hp -= _Att;
+    }
+
+public:
+    int Att = 0;
+    int Hp = 100;
+
+    int publicInt = 0;
+    void publicFunction()
+    {
+
+    }
+
+protected:
+    int protectedInt = 0;
+    void protectedFunction()
+    {
+
+    }
+
+private:
+    int privateInt = 0;
+    void privateFunction()
+    {
+
+    }
+
+};
+
+// 상속하기 (cf. 상속은 헤더를 추가해야한다.)
+// 그 기능을 물려받기 원하는 클래스 이름
+// 오른쪽에 : 접근제한지정자 부모클래스 이름
+class Player : public FightUnit
+{
+    
+};
+
+// 상속에서 접근제한 지정자
+// 내부의 기존 접근제한 지정자와 비교해 좁은 범위로 바꿈
+
+//              부모      자식      외부
+// public
+// protected
+// private
+
+class Monster : public FightUnit
+{
+    void Func()
+    {
+        
+    }
+};
+
+// 상속관계를 처음부터 생각하는 것은 별로 좋지않다.
+// 생각만하다 진행을 못한다.
+
+int main()
+{
+    Player NewPlayer = Player();
+    //NewPlayer.Att = 10;
+    //NewPlayer.Damage(20);
+
+    Monster NewMonster = Monster();
+    //NewMonster.Att = 5;
+    //NewMonster.Damage(10);
+}
