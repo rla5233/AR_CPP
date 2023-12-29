a2adedca39eb973fa0e067e8630b6693b0e50fd6
rla5233 <rla5233@naver.com>
2023-12-29 11:01:15 +0900

    061_virtual(가상함수) 중간

---
+++ Cpp/061_Virtual/061_Virtual.cpp
@@ -0,0 +1,59 @@
+﻿// 061_Virtual.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+class FightUnit
+{
+public:
+	// virtual 문법
+	// 자식이 만약 나와 완전히 동일한 함수를 구현한다면
+	// 자식의 함수를 우선적으로 실행한다.
+	virtual int GetAtt() { return Att; }
+
+private:
+	int Att = 10;
+
+};
+
+class Monster : public FightUnit
+{
+
+};
+
+class Player : public FightUnit
+{
+protected:
+	// override를 붙여서 부모에게서 물려받아 
+	// 재구현한 함수라는 것을 명시해준다. (안붙여도 동작하지만)
+	int GetAtt() override
+	{
+		return FightUnit::GetAtt() + WAtt;
+	}
+	
+private:
+	int WAtt = 5;
+};
+
+void Fight(FightUnit* _Left, FightUnit* _Right)
+{
+	// 겉으로 보기에는 FightUnit 이지만
+	// 동작할때는 플레이어나 몬스터처럼 동작하는것
+	// 그래서 다형성이라고 한다. 
+	int a = _Left->GetAtt();
+	int b = _Right->GetAtt();
+
+	int c = 0;
+}
+
+int main()
+{
+	// 가장 말단 형태로는 거의 사용하지 않는다.
+	Player NewPlayer;
+	Monster NewMonster;
+
+	Fight(&NewPlayer, &NewMonster);
+
+
+	return 0;
+}
--- Cpp/0_BJO/0_BJO.cpp
+++ Cpp/0_BJO/0_BJO.cpp
@@ -2,86 +2,88 @@
 #include <vector>
 using namespace std;
 
-class MiniFantasyWar
+class Problem
 {
 public:
-	MiniFantasyWar() {}
-
-	void SetCharacterCount() { cin >> m_CharacterCount; }
-	int GetCharacterCount()	{ return m_CharacterCount; }
+	void JBoxProblem();
 
 private:
-	int m_CharacterCount = 0;
 };
 
-class Character
+class JBox
 {
 public:
-	Character() {}
-	
-	void SetHp() { cin >> m_Hp; }
-	void SetMp() { cin >> m_Mp; }
-	void SetAtt() { cin >> m_Att; }
-	void SetArm() { cin >> m_Arm; }
-	void WearingEquipment()
-	{
-		int ChangeHp = 0, ChangeMp = 0, ChangeAtt = 0, ChangeArm = 0;
-		cin >> ChangeHp >> ChangeMp >> ChangeAtt >> ChangeArm;
+	void SetBoxVec();
+	void PrintJBox();
 
-		m_Hp += ChangeHp;
-		if (m_Hp < 1) { m_Hp = 1; }
-
-		m_Mp += ChangeMp;
-		if (m_Mp < 1) { m_Mp = 1; }
-
-		m_Att += ChangeAtt;
-		if (m_Att < 0) { m_Att = 0; }
-
-		m_Arm += ChangeArm;
-	}
-
-	void SetCombatPower() 
-	{
-		m_CombatPower = m_Hp + (5 * m_Mp) + (2 * m_Att) + (2 * m_Arm);
-	}
-
-	int GetCombatPower() { return m_CombatPower; }
+	inline void SetSize() { cin >> m_Size; }
 
 private:
-	int m_Hp = 0;
-	int m_Mp = 0;
-	int m_Att = 0;
-	int m_Arm = 0;
-
-	int m_CombatPower = 0;
+	int m_Size = 0;
+	vector<string> m_BoxVec = vector<string> ();
 };
 
-void Problem_12790();
 
 // 12790
 int main()
 {
 	//ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	Problem_12790();
+	
+	Problem NewProblem = Problem();
+	NewProblem.JBoxProblem();
+	
 
 	return 0;
 }
 
-void Problem_12790()
+void Problem::JBoxProblem()
 {
-	MiniFantasyWar NewGame = MiniFantasyWar();
-	NewGame.SetCharacterCount();
-	vector<Character> CharacterList(NewGame.GetCharacterCount(), Character());
+	int TestCase = 0;
+	cin >> TestCase;
 
-	for (int i = 0; i < CharacterList.size(); i++)
+	JBox NewJBox = JBox();
+
+	for (int i = 0; i < TestCase; i++)
 	{
-		CharacterList[i].SetHp();
-		CharacterList[i].SetMp();
-		CharacterList[i].SetAtt();
-		CharacterList[i].SetArm();
-		CharacterList[i].WearingEquipment();
-		CharacterList[i].SetCombatPower();
-		cout << CharacterList[i].GetCombatPower() << "\n";
+		NewJBox.SetSize();
+		NewJBox.SetBoxVec();
+		NewJBox.PrintJBox();
 	}
+}
+
+void JBox::SetBoxVec()
+{
+	m_BoxVec.clear();
+
+	string temp = "";
+
+	for (int i = 0; i < m_Size; i++)
+	{
+		for (int j = 0; j < m_Size; j++)
+		{
+			if (i == 0 || i == m_Size - 1 ||
+				j == 0 || j == m_Size - 1)
+			{
+				temp += '#';
+			}
+			else
+			{
+				temp += 'J';
+			}
+
+		}
+
+		m_BoxVec.push_back(temp);
+		temp = "";
+	}
+}
+
+void JBox::PrintJBox()
+{
+	for (int i = 0; i < m_BoxVec.size(); i++)
+	{
+		cout << m_BoxVec[i] << "\n";
+	}
+
+	cout << "\n";
 }
