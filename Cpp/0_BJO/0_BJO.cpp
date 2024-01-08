d01eda27c1c38f8efbeb4fee9f8ba723480b021c
rla5233 <rla5233@naver.com>
2024-01-08 12:48:13 +0900

    BJO 6603

--- Cpp/0_BJO/0_BJO.cpp
+++ Cpp/0_BJO/0_BJO.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
@@ -17,12 +18,13 @@ public:
 	Lotto() {}
 	~Lotto() {}
 
-	inline void SetK(int _K) { m_K = _K; }
-	void SetS_ByInput();
+	void AddNum_S(int _Num);
+	void Clear();
+	void PrintAllCase();
 
 private:
-	int m_K = 0;
-	string m_S = "";
+	const int m_Count = 6;
+	vector<int> m_SVec = vector<int>();
 };
 
 int main()
@@ -43,10 +45,60 @@ void Problem::Problem_6603(Lotto& _Lotto) const
 	{
 		int K = 0;
 		cin >> K;
-		_Lotto.SetK(K);
+		if (K == 0)
+		{
+			break;
+		}
 
+		for (int i = 0; i < K; i++)
+		{
+			int num = 0;
+			cin >> num;
+			_Lotto.AddNum_S(num);
+		}
+
+		_Lotto.PrintAllCase();
+		_Lotto.Clear();
 	}
 }
 
-void Lotto::SetS_ByInput()
-{}
+void Lotto::AddNum_S(int _Num)
+{
+	m_SVec.push_back(_Num);
+}
+
+
+void Lotto::Clear()
+{
+	m_SVec.clear();
+}
+
+void Lotto::PrintAllCase()
+{
+	for (int a = 0; a < (m_SVec.size() - m_Count) + 1; a++)
+	{
+		for (int b = a + 1; b < (m_SVec.size() - m_Count) + 2; b++)
+		{
+			for (int c = b + 1; c < (m_SVec.size() - m_Count) + 3; c++)
+			{
+				for (int d = c + 1; d < (m_SVec.size() - m_Count) + 4; d++)
+				{
+					for (int e = d + 1; e < (m_SVec.size() - m_Count) + 5; e++)
+					{
+						for (int f = e + 1; f < (m_SVec.size() - m_Count) + 6; f++)
+						{
+							cout << m_SVec[a] << " ";
+							cout << m_SVec[b] << " ";
+							cout << m_SVec[c] << " ";
+							cout << m_SVec[d] << " ";
+							cout << m_SVec[e] << " ";
+							cout << m_SVec[f] << "\n";
+						}
+					}
+				}
+			}
+		}
+	}
+
+	cout << "\n";
+}
