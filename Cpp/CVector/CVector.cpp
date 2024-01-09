00da28f3539b3a01a21e19880f628112005f6a93
rla5233 <rla5233@naver.com>
2024-01-09 15:51:30 +0900

    중간 정리

---
+++ Cpp/CVector/CVector.cpp
@@ -0,0 +1,26 @@
+﻿// CVector.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // 인터페이스?
+    // 클래스와 코드의 함수의 사용방식을 말함
+    // 익숙한 인터페이스
+    // 익숙한 클래스 사용방법
+    // 익숙한 클래스의 멤버변수들
+
+    std::vector<int> ArrVector = std::vector<int>(5);
+    ArrVector.resize(10);
+
+    // 자료 구조 3대장
+    // vector
+    // map
+    // list
+
+    // string
+    // queue
+    // stack
+}
