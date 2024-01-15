009cdb5ec3eabd255f76691ce92748527bdf3951
rla5233 <rla5233@naver.com>
2024-01-15 10:06:01 +0900

    BOJ 1543

--- Cpp/0_BJO/0_BJO.cpp
+++ Cpp/0_BJO/0_BJO.cpp
@@ -1,110 +1,30 @@
 ﻿#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <string>
 using namespace std;
 
-class Problem
-{
-public:
-	void Problem_12788(class IUPC& _IUPC) const;
-
-private:
-
-};
-
-class IUPC
-{
-public:
-	void SortCTPPen();
-	void BorrowPen();
-	
-	void SetCTPPen();
-	void SetCTP(int _Num);
-	void SetParticipent(int _Num);
-
-	inline bool CanBorrow() const { return m_CanBorrow; }
-	inline int GetBorrowCTP() const { return m_BorrowCTP; }
-
-private:
-	int m_CTP = 0;
-	int m_Participent = 0;
-	int m_BorrowCTP = 0;
-	bool m_CanBorrow = false;
-	vector<int> m_CTPPen = vector<int>();
-};
-
-
+// 1543
 int main()
 {
 	//ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	Problem NewProblem = Problem();
-	IUPC NewIUPC = IUPC();
-	NewProblem.Problem_12788(NewIUPC);
+	string doc = "", find = "";
+	getline(cin, doc);
+	getline(cin, find);
 
-	return 0;
-}
-
-void Problem::Problem_12788(IUPC& _IUPC) const
-{
-	int N = 0;
-	cin >> N;
-	_IUPC.SetCTP(N);
-
-	int M = 0, K = 0;
-	cin >> M >> K;
-	_IUPC.SetParticipent(M * K);
-
-	_IUPC.SetCTPPen();
-	_IUPC.SortCTPPen();
-
-	_IUPC.BorrowPen();
-
-	if (_IUPC.CanBorrow())
+	int count = 0, idx = 0;
+	while (true)
 	{
-		cout << _IUPC.GetBorrowCTP();
-	}
-	else
-	{
-		cout << "STRESS";
-	}
-}
-
-void IUPC::SetCTP(int _Num)
-{
-	m_CTP = _Num;
-}
-
-void IUPC::SetParticipent(int _Num)
-{
-	m_Participent = _Num;
-}
-
-void IUPC::SetCTPPen()
-{
-	m_CTPPen.assign(m_CTP, 0);
-	for (int i = 0; i < m_CTP; i++)
-	{
-		cin >> m_CTPPen[i];
-	}
-}
-
-void IUPC::SortCTPPen()
-{
-	sort(m_CTPPen.rbegin(), m_CTPPen.rend());
-}
-
-void IUPC::BorrowPen()
-{
-	int count = 0;
-	m_CanBorrow = false;
-	for (int i = 0; i < m_CTPPen.size(); i++)
-	{
-		count += m_CTPPen[i];
-		if (count >= m_Participent)
+		if (doc.find(find, idx) != string::npos)
 		{
-			m_BorrowCTP = i + 1;
-			m_CanBorrow = true;
-			return;
+			idx = static_cast<int>(doc.find(find, idx) + find.length());
+			++count;
+		}
+		else
+		{
+			cout << count;
+			break;
 		}
 	}
+
+	return 0;
 }
